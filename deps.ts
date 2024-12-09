5169b8ed95f5d285940d3601393f1ade68529064
creed <arnab.purkayastha@colorado.edu>
2024-12-09 11:42:55 -0500

    Update to arnabp superdeno fork

--- deps.ts
+++ deps.ts
@@ -1,5 +1,5 @@
 export { Server } from "https://deno.land/std@0.213.0/http/server.ts";
-export { superdeno, Test } from "https://deno.land/x/superdeno@4.9.0/mod.ts";
+export { superdeno, Test } from "https://raw.githubusercontent.com/arnabp/superdeno/main/mod.ts";
 export type {
   IRequest,
   IResponse,
